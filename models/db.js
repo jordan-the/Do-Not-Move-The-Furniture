7041142dce907b71bdad4a47675826c7551f134b
Qifan Tang <q.tang2@student.unimelb.edu.au>
2019-09-05 22:40:03 +0800

    deleted unused code and modified connection to database

--- models/db.js
+++ models/db.js
@@ -1,13 +1,11 @@
 // Create Database
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://mike:miketang@ds115340.mlab.com:15340/qifandb', function(err){
-    if(!err) {
-        console.log('Connected to mongo');
-    } else {
-        console.log('Failed to connect to mongo');
-    }
-});
+try {
+    mongoose.connect('mongodb+srv://qifan:donotmovethefurniture@cluster0-wdsdl.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
+} catch (error) {
+    console.log('fall to connect to mongodb');
+}
 
 //require the schemas
 require('./user.js');
--- models/story.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var storySchema = mongoose.Schema(
-    {
-       title: String,
-       story: String,
-       author: String
-    }
-);
-module.exports = mongoose.model('Story', storySchema);
