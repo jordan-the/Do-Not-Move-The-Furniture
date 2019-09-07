7933b34514be236c2b4f3c98a0e5ae51165e0a31
Qifan Tang <q.tang2@student.unimelb.edu.au>
2019-09-07 12:01:24 +0800

    adding category schema

---
+++ models/category.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const categorySchema = mongoose.Schema(
+    {
+        name: String
+    }
+);
+
+
+module.exports = mongoose.model('Category', categorySchema);
