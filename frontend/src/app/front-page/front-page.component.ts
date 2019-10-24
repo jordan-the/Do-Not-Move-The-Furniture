f2f1d4af508757597f4e58d8b4c325568be937a1
shan-dan <dshan@student.unimel.edu.au>
2019-10-25 03:34:03 +1100

    Front Page with slideshow

--- frontend/src/app/front-page/front-page.component.ts
+++ frontend/src/app/front-page/front-page.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, animate, transition, style } from '@angular/animations';
 
 @Component({
   selector: 'app-front-page',
   templateUrl: './front-page.component.html',
-  styleUrls: ['./front-page.component.css']
+  styleUrls: ['./front-page.component.css'],
+  animations: [
+    trigger('carousel', [
+    ])
+  ]
 })
 export class FrontPageComponent implements OnInit {
 
+  bannerPic: any[] = [
+      {img: '/assets/image/1.png'},
+      {img: '/assets/image/2.png'},
+      {img: '/assets/image/3.png'},
+    ];
+    state = 'stay';
+    timer: any;
+    
   constructor() { }
 
   ngOnInit() {
+    this.autoPlay();
   }
 
+  autoPlay(): void {
+    const me = this;
+    this.timer = setInterval(() => {
+      me.state = me.state === 'stay' ? 'moveLeft' : 'stay';
+    }, 3000);
+``}
+
+
+  afterPlay(): void {
+    if (this.state === 'stay' || this.state === 'moveLeft') {
+      this.bannerPic.push(this.bannerPic[0]);
+      this.bannerPic.shift();
+    } else if (this.state === 'moveRight') {
+      const last = this.bannerPic.length - 1;
+      this.bannerPic.unshift(this.bannerPic[last]);
+      this.bannerPic.pop();
+      this.state = 'stayR';
+    }
+  }
+
+  stopPlay(): void {
+    clearInterval(this.timer);
+  }
 }
